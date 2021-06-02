e2c15d8438613afb5fca5fe128d40c4f67079656
Benny <80082763+Benny25s@users.noreply.github.com>
2021-06-02 17:09:33 +0700

    Add files via upload

---
+++ PPL/application/controllers/control.php
@@ -0,0 +1,33 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Control extends CI_Controller {
+  
+  public function __construct(){
+    parent::__construct();
+    
+    $this->load->model('Model'); // Load Model ke controller ini
+
+  }
+  
+  public function index(){
+    $data['t_uas11189'] = $this->Model->view();
+    $this->load->view('index', $data);
+  }
+
+  public function jadwal(){
+    $data['t_uas11189'] = $this->Model->view();
+    $this->load->view('jadwal', $data);
+  }
+
+  public function peringkat(){
+    $data['t_uas11189'] = $this->Model->view();
+    $this->load->view('peringkat', $data);
+  }
+
+  public function berita(){
+    $data['t_uas11189'] = $this->Model->view();
+    $this->load->view('berita', $data);
+  }
+ 
+  
+
+}
